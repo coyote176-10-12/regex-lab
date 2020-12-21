1b15e14ff96a1a13cc442c0db8d9c29143cd323f
Javier <coyote176@msn.com>
2020-12-21 13:48:25 -0700

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -24,9 +24,7 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(phone)
-    if phone.match(/\d+.+\d.+\d/)
-        true
-    else
-        false
-    end
+     phone.match?(/\d+.+\d.+\d/)
+        
+    
 end
